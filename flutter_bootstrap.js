4f697b7053c4cb7e8d8c50a0dfcbd3081c12961c
devThiagoJorge <55405348+devThiagoJorge@users.noreply.github.com>
2025-09-23 01:09:44 +0000

    Deploying to gh-pages from @ devThiagoJorge/mensalidade-mts-app@0cbb64492b9d008ae9657bf5d94a5c49bdfb971a 🚀

--- flutter_bootstrap.js
+++ flutter_bootstrap.js
@@ -38,6 +38,6 @@ _flutter.buildConfig = {"engineRevision":"c29809135135e262a912cf583b2c90deb9ded6
 
 _flutter.loader.load({
   serviceWorkerSettings: {
-    serviceWorkerVersion: "4290641176"
+    serviceWorkerVersion: "857389716"
   }
 });
